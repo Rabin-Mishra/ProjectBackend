71091f2d4273af2add719099c9eb99fc45431656
karkiayush <karkiayush619@gmail.com>
2023-07-15 11:37:55 +0545

    order.php page ma sano code haru haleko  but orderid nai aaudeina jasle garda aagadi badna sakyena

--- GrocerEase-Website/users_area/checkout.php
+++ GrocerEase-Website/users_area/checkout.php
@@ -11,8 +11,8 @@ include('../functions/common_function.php');
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta> -->
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CheckOut Page</title>
     <!-- boootstrap css link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
